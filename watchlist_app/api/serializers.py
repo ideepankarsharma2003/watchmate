bfe5ea9a38ecfb67ad2ec77215e24861d7ebdc5c
Deepankar Sharma <deepankarsharma2003@gmail.com>
2025-03-06 21:45:13 +0530

    ADDED: field level validation, object level validation and validators

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,13 +2,18 @@ from rest_framework import serializers
 from ..models import Movie
 
 
+# validator function
+def year_validator(value):
+    if value < 1940:
+        raise serializers.ValidationError('Year should be greater than 1940!')
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=100)
     description = serializers.CharField()
     active = serializers.BooleanField(required=False)
     genre = serializers.CharField(max_length=100, required=False)
-    year = serializers.IntegerField(required=False)
+    year = serializers.IntegerField(required=False, validators=[year_validator, ])
     created_at = serializers.DateTimeField(required=False)
     updated_at = serializers.DateTimeField(required=False)
 
@@ -24,3 +29,15 @@ class MovieSerializer(serializers.Serializer):
         instance.year = validated_data.get('year', instance.year)
         instance.save()
         return instance
+    
+    # field level validation
+    def validate_title(self, value):
+        if len(value) <= 1:
+            raise serializers.ValidationError('Title is too short!')
+        return value
+    
+    # object level validation
+    def validate(self, data):
+        if (data['title']).lower() == data['description'].lower():
+            raise serializers.ValidationError('Title and Description should be different!')
+        return data
